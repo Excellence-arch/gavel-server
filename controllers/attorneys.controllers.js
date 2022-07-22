2b3485968de9e43ddc2b3028c5a23c5361334d4e
Dee <ajalaoyinda@gmail.com>
2022-07-22 21:59:00 +0100

    encrypting password and jwt token

--- controllers/attorneys.controllers.js
+++ controllers/attorneys.controllers.js
@@ -1,11 +1,13 @@
 const attorneyModel = require("../models/attorneys.models");
-
+const bcryptjs= require('bcryptjs')
+const jwt = require('jsonwebtoken')
 const internalServerError = (res) => {
     res.status(501).send({status: false, message: "Internal server error"});
 }
 
 const register = (req, res) => {
     const attorney = req.body;
+
     attorneyModel.findOne({email: attorney.email}, (err, resp) => {
         if(err) {
             internalServerError(res);
@@ -13,25 +15,57 @@ const register = (req, res) => {
             if(resp) {
                 res.send({status: false, message: "Email already exist"});
             } else {
+                const myPlaintextPassword = attorney.password
+                const salt = bcryptjs.genSaltSync(10);
+                const hash = bcryptjs.hashSync(myPlaintextPassword, salt);
+                // console.log(hash)
+                const newForm={
+                    first_name:attorney.first_name,
+                    last_name:attorney.last_name,
+                    email:attorney.email,                
+                    password:hash
+                }
+                console.log(newForm)
+                const form = new attorneyModel(newForm);
                 form.save((err) => {
                     res.send({status: true, message: "Registration successful"});
                 })
             }
         }
     })
-    const form = new attorneyModel(attorney);
 }
 
 const login = (req, res) => {
     const attorney = req.body;
-    attorneyModel.findOne({email: attorney.email}, (err, result) => {
+    const emaill=attorney.email
+    console.log(attorney)
+        attorneyModel.findOne({email: attorney.email}, (err,result) => {
+        console.log(result);
         if (err) internalServerError(res);
+        if (!result) {
+                console.log("No user found")
+                res.send({status: false, message: `No user found`});
+
+        }
         else {
-            if(result && result.password == attorney.password) {
-                res.send({status: true, message: `Login successful`});
-            } else {
-                res.send({status: false, message: `Invalid email or password`});
-            }
+            const receivedPassword=result.password;
+            const myPlaintextPassword = attorney.password;
+
+            bcryptjs.hash(myPlaintextPassword, 10).then((hash) => {
+                return bcryptjs.compare(myPlaintextPassword, receivedPassword)
+            }).then((result) => {
+                if(result==true){
+                    jwt.sign({emaill},  process.env.JWT_SECRET, function(err, token) {
+                        console.log(token);
+                        console.log("Successful login")
+                        res.send({message:"Your login is successful!",result,token})
+                    })
+                    }
+                    else {
+                        res.send({status: false, message: `Invalid email or password`});
+                        console.log("Wrong password")
+                    }
+                    });
         }
     })
 }
--- routes/attorney.routes.js
+++ routes/attorney.routes.js
@@ -1,7 +1,9 @@
-const { register } = require("../controllers/attorneys.controllers");
+const { register,login } = require("../controllers/attorneys.controllers");
 
 const route = require("express").Router();
 
 route.post("/register", register);
+route.post("/login", login);
+
 
 module.exports = route;
